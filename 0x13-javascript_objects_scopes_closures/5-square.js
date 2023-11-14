9bbec94fdbf6eecb471f9a0d889babfa4f258ed9
edwin-pr <edwingitau100@gmail.com>
2023-11-14 06:07:47 -0800

    “updates”

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Square class that inherits from rectangle class
+ */
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+// Call the constructor of Rectangle with size as both width and height
+    super(size, size);
+  }
+}
+
+module.exports = Square;
