015ef339a83f44dce9064e0095c0ac7b733dce07
edwin-pr <edwingitau100@gmail.com>
2023-11-30 02:24:03 -0800

    “updates”

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL as an argument."
-  exit 1
-fi
-
-# Send request to the URL and store the response in a variable
-response=$(curl -sI "$1")
-
-# Extract the Content-Length header from the response
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}')
-
-# Display the size of the body of the response in bytes
-echo "$content_length"
+# sends a request to the URL at $1 and displays the size of the body of the response
+curl -s "$1" | wc -c
 
