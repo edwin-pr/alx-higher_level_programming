1982afd35f27f4b20e49c8076e0ad1d71e7c77ec
edwin-pr <edwingitau100@gmail.com>
2023-11-30 10:43:03 +0000

    “updates”

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# sends a request to the URL at $1 and displays the size of the body of the response
-curl -s "$1" | wc -c
+# sends a request to the URL at $1 and displays the size of the body of the responce
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
 
